a5c11673163d1e8c987d57478b01635a76cd7fe4
gk-google@ninebynine.org <gk-google@ninebynine.org@d087ce6c-597e-11de-8832-3f4c773c73ae>
2009-09-07 15:25:31 +0000

    Started on test case for loadworkspace

---
+++ test/test-shuffl-loadworkspace.js
@@ -0,0 +1,28 @@
+// $Id$
+
+/**
+ * Test suite for workspace loading function
+ * 
+ * Dependencies:
+ *  test-shuffl-loadworkspace-layout.json
+ *  test-shuffl-loadworkspace-card_1.json
+ *  test-shuffl-loadworkspace-card_2.json
+ */
+
+/**
+ * Function to register tests
+ */
+
+TestLoadWorkspace = function() {
+
+    module("TestLoadWorkspace");
+
+    test("shuffl.LoadWorkspace",
+        function () {
+            log.debug("test shuffl.LoadWorkspace");
+            ok(false, "TODO");
+        });
+
+};
+
+// End
