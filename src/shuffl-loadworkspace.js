7d47a6e812eb4808a0e65a037337e804d35ae5ca
gk-google@ninebynine.org <gk-google@ninebynine.org@d087ce6c-597e-11de-8832-3f4c773c73ae>
2009-10-08 08:52:45 +0000

    Add copyright and licence details to source code.

--- src/shuffl-loadworkspace.js
+++ src/shuffl-loadworkspace.js
@@ -1,9 +1,21 @@
 /**
  * @fileoverview
- *  Shuffl code to load card data into the workspace.
+ *  Shuffl code to load cards into the workspace.
  *  
  * @author Graham Klyne
  * @version $Id$
+ * 
+ * Coypyright (C) 2009, University of Oxford
+ *
+ * Licensed under the MIT License.  You may obtain a copy of the License at:
+ *
+ *     http://www.opensource.org/licenses/mit-license.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 // TODO: refactor this code to accept a storage session object, so the
