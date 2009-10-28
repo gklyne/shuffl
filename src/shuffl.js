7478d31dd53bf223de85dcc58aa0b5ae646598b3
gk-google@ninebynine.org <gk-google@ninebynine.org@d087ce6c-597e-11de-8832-3f4c773c73ae>
2009-10-28 06:57:37 +0000

    Move menu and dialog definitions from HTML pages into main Shuffl module.

--- src/shuffl.js
+++ src/shuffl.js
@@ -147,36 +147,77 @@ shuffl.menuSaveNewWorkspace = function () {
 };
 
 // ----------------------------------------------------------------
+// Add menus and dialogs HTML templates
+// ----------------------------------------------------------------
+
+shuffl.MainMenuHTML =
+    "<div class='contextMenu' id='workspacemenuoptions'>\n"+
+    "  <ul>\n"+
+    "    <li id='open'><img src='folder.png' />Open workspace...</li>\n"+
+    "    <li id='save'><img src='folder.png' />Save workspace</li>\n"+
+    "    <li id='savenew'><img src='folder.png' />Save as new workspace...</li>\n"+
+    "  </ul>\n"+
+    "</div>\n";
+
+shuffl.OpenDialogHTML =
+    "<div id='dialog_open' title='Open workspace' style='display:none;'>\n"+
+    "  <p>Enter the URI of an Atom Publishing Protocol service, AtomPub feed path and workspace name where the workspace is to be loaded from.</p>\n"+
+    "  <form>\n"+
+    "    <fieldset>\n"+
+    "      <legend>Location of workspace data</legend>\n"+
+    "      <label for='open_atomuri'>AtomPub service URI:</label>\n"+
+    "      <input type='text' name='atomuri' id='open_atomuri' class='text ui-widget-content ui-corner-all' size='80'/>\n"+
+    "      <label for='open_feedpath'>Atom feed path:</label>\n"+
+    "      <input type='text' name='feedpath' id='open_feedpath' class='text ui-widget-content ui-corner-all' size='80'/>\n"+
+    "      <label for='open_wsname'>Workspace name:</label>\n"+
+    "      <input type='text' name='wsname' id='open_wsname' class='text ui-widget-content ui-corner-all' size='80'/>\n"+
+    "    </fieldset>\n"+
+    "  </form>\n"+
+    "</div>\n";
+
+shuffl.SaveNewDialogHTML =
+    "<div id='dialog_save' title='Save as new workspace' style='display:none;'>\n"+
+    "  <p>Enter the URI of an Atom Publishing Protocol service, AtomPub feed path and workspace name where the new workspace is to be saved.</p>\n"+
+    "  <form>\n"+
+    "    <fieldset>\n"+
+    "      <legend>Location for saved workspace</legend>\n"+
+    "      <label for='save_atomuri'>AtomPub service URI:</label>\n"+
+    "      <input type='text' name='atomuri' id='save_atomuri' class='text ui-widget-content ui-corner-all' size='80'/>\n"+
+    "      <label for='save_feedpath'>Atom feed path:</label>\n"+
+    "      <input type='text' name='feedpath' id='save_feedpath' class='text ui-widget-content ui-corner-all' size='80'/>\n"+
+    "      <label for='save_wsname'>Workspace name:</label>\n"+
+    "      <input type='text' name='wsname' id='save_wsname' class='text ui-widget-content ui-corner-all' size='80'/>\n"+
+    "    </fieldset>\n"+
+    "  </form>\n"+
+    "</div>\n";
+
+// ----------------------------------------------------------------
 // Start-up logic
 // ----------------------------------------------------------------
 
 jQuery(document).ready(function() {
-
     log.info("shuffl starting");
 
-    /**
-     * Attach card-creation functions to stockpile cards
-     */    
+    // Add menus and dialogs to the workspace
+    jQuery("body").append(shuffl.MainMenuHTML);
+    jQuery("body").append(shuffl.OpenDialogHTML);
+    jQuery("body").append(shuffl.SaveNewDialogHTML);
+
+    // Attach card-creation functions to stockpile cards
     log.debug("shuffl: attach card-creation functions to stockpile");
     jQuery("div.shuffl-stockpile").data( 'makeCard', shuffl.createCardFromStock);
 
-    /**
-     * Size workspace to fit within window (by default, it doesn't on Safari)
-     */
+    // Size workspace to fit within window (by default, it doesn't on Safari)
     log.debug("shuffl: attach window resize handler)");
     jQuery(window).resize( shuffl.resize );
     shuffl.resize();
 
-    /**
-     * Connect up drag and drop for creating and moving cards
-     * Only cards predefined in the original HTML are hooked up here
-     */
+    // Connect up drag and drop for creating and moving cards
+    // Only cards predefined in the original HTML are hooked up here
     log.debug("shuffl: connect drag-and-drop logic");
-
     jQuery("div.shuffl-stockpile").draggable(shuffl.stockDraggable);
     jQuery("div.shuffl-card").draggable(shuffl.cardDraggable);
     jQuery("div.shuffl-card").click( function () { shuffl.toFront(jQuery(this)) } );
-
     jQuery("#layout").droppable({
         accept: "div.shuffl-stockpile",
         drop: 
@@ -192,14 +233,10 @@ jQuery(document).ready(function() {
             }
         });
     
-    /**
-     * TODO: connect up logic for saving changes to backend store
-     */    
+    // TODO: connect up logic for saving changes on-the-fly to backend store
     log.debug("shuffl TODO: connect content save logic");
 
-    /**
-     * Create a pop-up workspace menu
-     */    
+    // Create a pop-up workspace menu
     log.debug("shuffl: connect connect workspace menu");
     jQuery('div.shuffl-workspacemenu').contextMenu('workspacemenuoptions', {
         menuStyle: {
@@ -224,13 +261,9 @@ jQuery(document).ready(function() {
                 }
           }
       });
-    
-    /**
-     * Initialization is done - now it's all event-driven
-     */
-    
-    log.info("shuffl initialization done");
 
+    // Initialization is done - now it's all event-driven
+    log.info("shuffl initialization done");
     });
 
 // End.
