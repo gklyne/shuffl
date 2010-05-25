e8d3da408799bcf0034332639555ffcc3398210a
gk-google@ninebynine.org <gk-google@ninebynine.org@d087ce6c-597e-11de-8832-3f4c773c73ae>
2010-05-25 18:04:28 +0000

    Refactored common code in sample apps, and hence updated them to run with WebDAV storage regime.

---
+++ static/apps/shuffl-sample-common.js
@@ -0,0 +1,41 @@
+/**
+ * @fileoverview
+ *  Shuffl sample applications common code.
+ *  
+ * @author Graham Klyne
+ * @version $Id: $
+ * 
+ * Coypyright (C) 2009, University of Oxford
+ *
+ * Licensed under the MIT License.  You may obtain a copy of the License at:
+ *
+ *     http://www.opensource.org/licenses/mit-license.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+shuffl.sampleLoadWorkspace = function (wsname)
+{
+    jQuery(document).ready(function(){
+        shuffl.addStorageHandler( 
+            { uri:      "http://localhost:8080/exist/atom/edit/"
+            , name:     "eXistAtomPub"
+            , factory:  shuffl.ExistAtomStorage
+            });
+        shuffl.addStorageHandler( 
+            { uri:      "http://localhost/webdav/shuffl/"
+            , name:     "WebDAV-shuffl"
+            , factory:  shuffl.WebDAVStorage
+            });
+        shuffl.addStorageHandler( 
+            { uri:      "http://localhost/webdav/shuffltest/"
+            , name:     "WebDAV-shuffltest"
+            , factory:  shuffl.WebDAVStorage
+            });
+        shuffl.loadWorkspace(wsname, shuffl.saveWorkspaceDefaults);
+    });
+}; 
